96e4545d765cecb783ea9534b1d2adddc0f02ad4
Jono Matusky <jono.matusky@gmail.com>
2021-05-24 15:20:10 -0400

    Add inline prop

--- src/components/CardVideo.js
+++ src/components/CardVideo.js
@@ -54,6 +54,7 @@ const CardVideo = ({ card, style, preview }) => {
                 width="100%"
                 overflow="hidden"
                 controls={false}
+                playsinline={true}
                 playing={isPlaying}
               />
               <Box
