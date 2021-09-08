0aca06431985eaba726d03bc749c09c24a52c674
Jono Matusky <jono.matusky@gmail.com>
2021-09-08 14:34:07 -0400

    Add AR landing page

--- src/App.js
+++ src/App.js
@@ -40,6 +40,8 @@ import OnTheWaitlist from 'pages/OnTheWaitlist/OnTheWaitlist'
 import Recover from 'pages/Recover/Recover'
 import Help from 'pages/Help/Help'
 import Subscribe from 'pages/Subscribe/Subscribe'
+import LandingAR from 'pages/LandingAR/LandingAR'
+import LandingARDemo from 'pages/LandingARDemo/LandingARDemo'
 
 const { REACT_APP_POSTHOG_KEY } = process.env
 
@@ -74,6 +76,11 @@ const App = () => {
       <Route path="/register">
         <Register />
       </Route>
+
+      <Route path="/ar" exact>
+        <LandingARDemo />
+      </Route>
+
       <Redirect path="/signup" exact to="/register" />
 
       <Route path="/s/subscribe">
@@ -85,6 +92,10 @@ const App = () => {
       <Route path="/s/help">
         <Help />
       </Route>
+      <Route path="/s/ar" exact>
+        <LandingAR />
+      </Route>
+
       <Route path="/s/invite-code" exact>
         <SignUpWithCode />
       </Route>
